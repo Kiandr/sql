6f0061fd02c807cce4dcd26eeaab9a1b2444b957
Kian Davoudi-Rad <kiandr@KIAN.hitronhub.home>
2018-09-11 20:31:18 -0700

    adding some changes to the script

--- Db/DataCamp/InnerJoinAlias3.sql
+++ Db/DataCamp/InnerJoinAlias3.sql
@@ -1,4 +1,11 @@
-select cities.name as city, countries.name as country, countries.region
-from cities  
-inner join countries  
-on cities.country_code = countries.code
+select c.code, c.name, c.region, p.year, p.fertility_rate
+from countries as c
+inner join populations as p
+on c.code  = p.country_code 
+
+
+SELECT c.code, name, region, e.year, fertility_rate, unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+INNER JOIN economies AS e;
