b67262b6360b371119290ac8e0c70a7f52621e70
Kian Davoudi-Rad <kiandr@Kians-MBP.localdomain>
2018-09-13 07:46:19 -0700

    adding self join practice

---
+++ Db/DataCamp/SelfJoin.sql
@@ -0,0 +1,16 @@
+SELECT p1.country_code, 
+       p1.size AS size2010,
+       p2.size AS size2015
+FROM populations AS p1
+inner JOIN populations AS p2
+ON  p1.pop_id = p2.pop_id;
+
+
+SELECT p1.country_code,
+       p1.size AS size2010, 
+       p2.size AS size2015,
+       ((p2.size - p1.size)/p1.size * 100.0) AS growth_perc
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code
+    AND p1.year = p2.year - 5;
