f1b64c970b555415a35f16779b93093d8f5a4e5f
kiandr <kian.rad@enlivenfinancial.com>
2018-09-18 10:22:15 -0700

    case

--- Db/DataCamp/Case.sql
+++ Db/DataCamp/Case.sql
@@ -25,3 +25,21 @@ FULL JOIN languages
 USING (code)
 WHERE countries.name LIKE 'V%' OR countries.name IS NULL
 ORDER BY countries.name;
+
+SELECT *
+FROM countries AS country
+FULL JOIN languages AS language
+USING (code)
+FULL JOIN currencies AS curency
+USING (code)
+WHERE region LIKE 'M%esia';
+
+
+SELECT c1.name AS country, region, l.name AS language,
+       basic_unit, frac_unit
+FROM countries AS c1
+FULL JOIN languages AS l
+USING (code)
+FULL JOIN currencies AS c2
+USING (code)
+WHERE region LIKE 'M%esia';
